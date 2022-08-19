47c229e1f74a8fe96eeb1dd7b1276e37ca90dddd
Sujana Bhandari <sujana6589@gmail.com>
2022-08-19 09:46:25 +0200

    Dynamically fetch the authors and blogtypes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import logo from './logo.svg';
 import Article from './Components/Article';
 import Articles from './Components/Articles';
-import Author from './Components/Author';
 import './App.css';
 import { client } from './main';
 import Nav from './Components/Nav';
 import { NavLink, Routes, Route } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import '../src/styles/main.css'
+import Blogs from './Components/Blogs';
+import Author from './Components/Author';
+import Details from './Components/Details'
+import BlogTypes from './Components/BlogTypes';
+import BlogTypes_Card from './Components/BlogTypes_Card';
+
+import Categories from './Components/Categories';
 
 function App() {
 
   const [articles, setArticles] = useState([]);
   const [newArticles, setNewArticles] = useState([]);
-  const [query, setQuery] = useState("")
+
+  const [query, setQuery] = useState("");
+  const [navItems, setNavItems] = useState({});
+
 
   useEffect(() => {
     client.getEntries().then(function (entries) {
       setArticles(entries.items);
       setNewArticles(entries.items);
     });
-  
+
   }, [])
 
   useEffect(() => {
@@ -30,12 +38,30 @@ function App() {
         //if query is empty
         console.log("empty")
         return setNewArticles(articles)
-      } else if(post.fields.name.toLowerCase().includes(query.toLowerCase()) || post.fields.author.toLowerCase().includes(query.toLowerCase())){
+      } else if (post.fields.name.toLowerCase().includes(query.toLowerCase()) || post.fields.author.toLowerCase().includes(query.toLowerCase())) {
         array.push(post);
         return setNewArticles(array)
       }
     });
   }, [query])
+
+  useEffect(() => {
+    const navbarItems = {
+      "authors": [],
+      "blogTypes": []
+    };
+    const authors = new Set();
+    const blogTypes = new Set();
+    articles.forEach(article => {
+      authors.add(article.fields.author);
+      blogTypes.add(article.fields.blogtype);
+    });
+    navbarItems.authors = [...authors];
+    navbarItems.blogTypes = [...blogTypes];
+    setNavItems(navbarItems);
+  }, [articles])
+
+
 
   return (
     <div className="App">
@@ -43,15 +69,21 @@ function App() {
         <NavLink to="/" className="link"><h1>Contentful Blog</h1></NavLink>
         <p>Welcome to our blog</p>
         <input placeholder="Search" onChange={event => setQuery(event.target.value)} />
-        {/* <Nav /> */}
+        <Categories navItems={navItems} />
       </header>
 
       <Routes>
-        <Route path='/' element={<Articles articles={newArticles}/>} /> 
-        <Route path=':id' element={<Article articles={newArticles}/>} /> 
-        <Route path='Author/Christian' element={<Author articles={newArticles} author='Christian'/>} /> 
-        <Route path='Author/Sujana' element={<Author articles={newArticles} author='Sujana Bhandari'/>} /> 
-        <Route path='Author/Robert' element={<Author articles={newArticles} author='Robert'/>} /> 
+        <Route path='/' element={<Articles articles={newArticles} />} />
+        <Route path='articles' element={<Articles articles={newArticles} />} />
+
+        <Route path='articles/:id' element={<Article articles={newArticles} />} />
+
+        <Route path='authors' element={<Author articles={articles} />} />
+
+        <Route path='authors/:author_name' element={<Details articles={articles} />} />
+
+        <Route path='blogtypes' element={<BlogTypes articles={articles} />} />
+        <Route path='blogtypes/:type_name' element={<BlogTypes_Card articles={articles} />} />
       </Routes>
 
     </div>
